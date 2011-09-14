935c1c87e70b6d666ac3997a51c0f31bc50b3a57
Giso Stallenberg <giso@connectholland.nl>
2011-09-14 10:49:28 +0000

    Prototype-1.7.ch: 'rake test'-build of https://github.com/sstephenson/prototype/tree/1.7
    git-svn-id: file:///srv/svn/main/prototype/test/1.7.ch@52633 3e26f76b-fb05-0410-91fc-a0551e8366c8

---
+++ vendor/unittest_js/lib/unittest_js/browsers/abstract.rb
@@ -0,0 +1,71 @@
+module UnittestJS
+  module Browser
+    class Abstract
+      def supported?
+        true
+      end
+
+      def setup
+      end
+
+      def teardown
+      end
+
+      def host
+        require 'rbconfig'
+        Config::CONFIG['host']
+      end
+
+      def macos?
+        host.include?('darwin')
+      end
+
+      def windows?
+        host.include?('mswin')
+      end
+
+      def linux?
+        host.include?('linux')
+      end
+      
+      def visit(url)
+        if macos?
+          system("open -g -a #{path} '#{url}'")
+        elsif windows?
+          system("#{path} #{url}")
+        elsif linux?
+          system("#{name} #{url}")
+        end
+      end
+      
+      def installed?
+        if macos?
+          File.exists?(path)
+        else
+          true #TODO
+        end
+      end
+      
+      def name
+        n = self.class.name.split('::').last
+        linux? ? n.downcase : n
+      end
+      
+      def escaped_name
+        name.gsub(' ', '\ ')
+      end
+      
+      def path
+        if macos?
+          File.expand_path("/Applications/#{escaped_name}.app")
+        else
+          @path
+        end
+      end
+      
+      def to_s
+        name
+      end
+    end
+  end
+end
